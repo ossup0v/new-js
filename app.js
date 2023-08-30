0d4ee3db146233922d901d0726d55e41b12df6f9
mikhail <ossup0v@gmail.com>
2023-08-31 01:00:43 +0400

    init commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const app = Vue.createApp({
+    //template: '<h2>Im template</h2>'
+    data() {
+        return {
+            show: true,
+            url: 'https://google.com',
+            testField: 'YEASSSSS TEST FIELD',
+            counts: 1,
+            show1: true,
+            show2: false,
+            show3: false,
+            show4: false,
+            x: null,
+            y: null,
+            XYtext: null,
+            exspenses: [
+                { category: 'eat', amountUSD: 25, imagePath: 'assets/eat.png', isFavorite:true },
+                { category: 'rent', amountUSD: 850, imagePath: 'assets/rent.png', isFavorite:false },
+                { category: 'taxi', amountUSD: 200, imagePath: 'assets/taxi.png', isFavorite:true },
+                { category: 'invest', amountUSD: -50, imagePath: 'assets/invest.png', isFavorite:true },
+                { category: 'other', amountUSD: 9999, imagePath: 'assets/other.png', isFavorite:false },
+            ]
+        }
+    },
+    methods: {
+        changeTestFiled(newValue) {
+            this.testField = newValue
+        },
+        changeShowState() {
+            this.show = !this.show
+        },
+        increateCounts() {
+            this.counts++;
+        },
+        handleMouseEvent(e, a){
+            console.log(e, e.type)
+            if(a) {
+                console.log(a)
+            }
+        },
+        handleMouseMoveEvent(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+            this.XYtext = this.x + ' : ' + this.y
+        },
+        toggleFavorite(exspense){
+            exspense.isFavorite = !exspense.isFavorite 
+        }
+    },
+    computed: {
+        favoriteExspenses() {
+            return this.exspenses.filter(x=>x.isFavorite)
+        },
+        notFavoriteExspenses() {
+            return this.exspenses.filter((x) => !x.isFavorite);
+        }
+    }
+})
+
+app.mount('#app')
