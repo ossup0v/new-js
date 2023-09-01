804e8558ac43408651de9b3fb1d2a8a90ad5421f
mikhail <ossup0v@gmail.com>
2023-09-02 00:41:36 +0400

    some updates

--- new-vue/tracker/src/App.vue
+++ new-vue/tracker/src/App.vue
@@ -5,7 +5,28 @@
     </div>
   </h1>
   <br>
-  <Modal />
+  <div v-if="showModal">
+    <Modal :counter="counter" :theme="getTheme" @close="modalToggle">
+      <template v-slot:links>
+        <a href="#">Sign up</a>
+        <a href="#">More info</a>
+      </template>
+      <h1>HElloo header</h1>
+      <p>some additional text</p>
+    </Modal>
+  </div>
+  <div v-if="showModalSecond">
+    <Modal :counter="counter" :theme="getTheme" @close="modalToggleSecond">
+      <template v-slot:links>
+        <a href="#">Only one link!</a>
+      </template>
+      <h1>Very diffent modal here!</h1>
+    </Modal>
+  </div>
+  <button @click="increaseCounter" class="button-62">click me!</button>
+  <button @click="saleToggle" class="button-62">Sale?</button>
+  <button @click="modalToggle" class="button-62">Show modal</button>
+  <button @click="modalToggleSecond" class="button-62">Show second modal</button>
 </template>
 
 <script>
@@ -16,7 +37,37 @@ export default {
   components: { Modal },
   data() {
     return {
-      title: 'VERY LOOOOOONG TEXTXXXXXXTTTT'
+      title: 'VERY LOOOOOONG TEXTXXXXXXTTTT',
+      modalHeader: 'TEST HeAdEr',
+      modalText: 'And some text here',
+      counter: 0,
+      showModal: false,
+      showModalSecond: false,
+      isSale: true,
+    }
+  },
+  methods: {
+    increaseCounter() {
+      this.counter++;
+    },
+    modalToggle(){
+      this.showModal = !this.showModal
+    },
+    modalToggleSecond(){
+      this.showModalSecond = !this.showModalSecond
+    },
+    saleToggle(){
+      this.isSale = !this.isSale
+    }
+  },
+  computed: {
+    getTheme() {
+      if(this.isSale){
+        return 'sale'
+      }
+      else{
+        return 'noSale'
+      }
     }
   }
 }
@@ -32,9 +83,49 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+button {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    margin: 10px;
+    text-decoration: none;
+    stroke: 1px #333;
+  }
 h1 {
   border-bottom: 5px solid #eb4b4b;
   display: inline-block;
   padding-bottom: 10px;
+}
+
+/* CSS */
+.button-62 {
+  background: linear-gradient(to bottom right, #EF4765, #FF9A5A);
+  border: 0;
+  border-radius: 12px;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system,system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 2.5;
+  outline: transparent;
+  padding: 0 1rem;
+  text-align: center;
+  text-decoration: none;
+  transition: box-shadow .2s ease-in-out;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+}
+
+.button-62:not([disabled]):focus {
+  box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+}
+
+.button-62:not([disabled]):hover {
+  box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
 }
 </style>
--- new-vue/tracker/src/components/Modal.vue
+++ new-vue/tracker/src/components/Modal.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="backdrop">
-    <div class="modal">
-      <h1>HEADER</h1>
-      <p>modal content</p>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{sale: theme === 'sale'}">
+      <p>You clicked {{ counter }} times!</p>
+      <slot>default content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'mod'
+  name: 'mod',
+  props: [ 'counter', 'theme' ],
+  methods: {
+    closeModal() {
+      this.$emit('close')
+    }
+  }
 }
+
 </script>
-<style scoped>
+<style>
   .modal {
     width: 400px;
     padding: 20px;
@@ -27,10 +37,41 @@ export default {
     width: 100%;
     height: 100%;
   }
-  h1 {
-    color: blue;
-  }
   .modal p {
     font-style: normal;
+  }
+  
+  .modal.actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+  }
+
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
+
+  .modal.sale {
+    background-color: red;
+    color: white;
+  }
+
+  .modal.sale p {
+    color: white;
+  }
+
+  .modal.sale h1 {
+    color: white;
+  }
+  .modal.sale .actions {
+    color: white;
+  }
+
+  .modal.sale .actions a {
+    color: white;
   }
 </style>
