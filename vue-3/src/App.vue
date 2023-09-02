549b15773c456d57f8a68f1bf62d6bd58eb19227
mikhail <ossup0v@gmail.com>
2023-09-03 01:41:16 +0400

    small update

---
+++ vue-3/src/App.vue
@@ -0,0 +1,87 @@
+<template>
+  <h1>Ninja reaction timer</h1>
+  <button @click="startPlay" :disabled="isPlaying" class="button-62">Play</button>
+    <Block :delay="delay" v-if="isPlaying" @end="endGame"/>
+    <Results v-if="showResults" :score="score" />
+    <!--<Results/>-->
+</template>
+
+<script>
+import Block from './components/Block.vue'
+import Results from './components/Results.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results
+  },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods: {
+    startPlay(){
+      this.delay = 500 + Math.random() * 5000
+      this.isPlaying = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
+    }
+  },
+  computed: {
+    getIsPlaying(){
+      return this.isPlaying
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+/* CSS */
+.button-62 {
+  background: linear-gradient(to bottom right, #EF4765, #FF9A5A);
+  border: 0;
+  border-radius: 12px;
+  color: #FFFFFF;
+  cursor: pointer;
+  display: inline-block;
+  font-family: -apple-system,system-ui,"Segoe UI",Roboto,Helvetica,Arial,sans-serif;
+  font-size: 16px;
+  font-weight: 500;
+  line-height: 2.5;
+  outline: transparent;
+  padding: 0 1rem;
+  text-align: center;
+  text-decoration: none;
+  transition: box-shadow .2s ease-in-out;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+  white-space: nowrap;
+}
+
+.button-62:not([disabled]):focus {
+  box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+}
+
+.button-62:not([disabled]):hover {
+  box-shadow: 0 0 .25rem rgba(0, 0, 0, 0.5), -.125rem -.125rem 1rem rgba(239, 71, 101, 0.5), .125rem .125rem 1rem rgba(255, 154, 90, 0.5);
+}
+</style>
---
+++ vue-3/src/components/Results.vue
@@ -0,0 +1,24 @@
+<template>
+    <p>Your reaction time {{ score }} ms!</p>
+    <div class="rank">
+        <div v-if="score < 250">Ninja fingers!</div>
+        <div v-else-if="score < 600">Rapid reaction!</div>
+        <div v-else>Too slow..</div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['score']
+}
+
+
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4rem;
+        color: #0faf87;
+        font-weight: bold;
+    }
+</style>
